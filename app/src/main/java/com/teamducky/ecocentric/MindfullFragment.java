6f0fd5540576bf50df6cb8a4c8cff871afea9cb1
Dol3v-Dab <dol12dab@gmail.com>
2020-10-10 17:05:42 +0300

    Created Task class, started working on Mindfulness fragment.

--- app/src/main/java/com/teamducky/ecocentric/MindfullFragment.java
+++ app/src/main/java/com/teamducky/ecocentric/MindfullFragment.java
@@ -1,5 +1,6 @@
 package com.teamducky.ecocentric;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MindfullFragment extends Fragment {
 
@@ -23,10 +26,23 @@ public class MindfullFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ViewGroup layout = (ViewGroup) getView().findViewById(R.id.TaskView);
-        Task[] usrTasks = {}; //TODO: Change from manual to user based
+        Task[] usrTasks = {new Task(5, "running", 500)}; //TODO: Change from manual to user based
         for (Task task :
                 usrTasks) {
-            LinearLayout taskLayout = new LinearLayout(); //do smth
+            LinearLayout taskLayout = new LinearLayout(getContext());
+            taskLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            taskLayout.setOrientation(LinearLayout.HORIZONTAL);
+            layout.addView(taskLayout);
+
+            RadioButton radioButton = new RadioButton(getContext());
+            TextView taskText = new TextView(getContext());
+//            taskText.setBackgroundColor(Color.parseColor("#d6dcc2")); //TODO: use drawable for text backgroumd, fix margins
+            // TODO: use montserrat font
+            taskText.setText(task.toString());
+
+            taskLayout.addView(radioButton);
+            taskLayout.addView(taskText);
         }
     }
 }
