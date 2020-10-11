64929ec01d9f2820041266b5a1f3268be6382730
nerya <neryasheetirt@gmail.com>
2020-10-11 13:01:57 +0300

    Added profile frag

--- app/src/main/java/com/teamducky/ecocentric/ProfileFragment.java
+++ app/src/main/java/com/teamducky/ecocentric/ProfileFragment.java
@@ -1,27 +1,88 @@
 package com.teamducky.ecocentric;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.WriterException;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 
 public class ProfileFragment extends Fragment {
 
+    private TextView nameTxt;
+    private ImageView QR;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        return v;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        nameTxt = getView().findViewById(R.id.nameDisplay);
+        nameTxt.setText(ParseUser.getCurrentUser().getUsername());
+        getView().findViewById(R.id.changePWDBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.requestPasswordResetInBackground(ParseUser.getCurrentUser().getEmail(), new RequestPasswordResetCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            Toast.makeText(getContext(),"Password reset mail was sent!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        QR = getView().findViewById(R.id.QRcodeImg);
+
+        QRGEncoder qrgEncoder = new QRGEncoder(ParseUser.getCurrentUser().getObjectId(), null, QRGContents.Type.TEXT, 400);
+        qrgEncoder.setColorBlack(Color.BLACK);
+        qrgEncoder.setColorWhite(Color.WHITE);
+        // Getting QR-Code as Bitmap
+        Bitmap bitmap = qrgEncoder.getBitmap();
+        // Setting Bitmap to ImageView
+        QR.setImageBitmap(bitmap);
+
+        getView().findViewById(R.id.logOutBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            Intent intent = new Intent(getContext(), MainActivity.class);
+                            startActivity(intent);
+                            getActivity().finish();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
